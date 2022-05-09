a63b2522a2f3de1ecb3adf47a60fc8b15772fa29
s-ashwin <ashwin.sivanagarajan@gmail.com>
2022-05-09 12:03:24 +0530

    Handles State between components

--- host/src/App.jsx
+++ host/src/App.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import RemoteComponent1 from "remote/Component1"
+import RemoteComponent1 from "remote/Component1";
 import "./index.scss";
 
-const Main = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: host</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
+const Main = () => {
+  const [state, setState] = useState(0);
+  return (
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
+      <div>Name: host</div>
+      <div>Framework: react</div>
+      <div>Language: JavaScript</div>
+      <div>CSS: Tailwind</div>
+      <div className="bg-purple-700 text-white px-5 my-5 py-2 rounded-lg">State: {state}</div>
+      <button
+        className="bg-blue-700 text-white px-5 py-2 rounded-lg"
+        onClick={() => setState(state + 1)}
+      >
+        Increment
+      </button>
 
-    <RemoteComponent1/>
-  </div>
-);
+      <RemoteComponent1 state={state} setState={setState} />
+    </div>
+  );
+};
 ReactDOM.render(<Main />, document.getElementById("app"));
--- remote1/src/Component1.jsx
+++ remote1/src/Component1.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
-export default function Component1() {
+export default function Component1({state, setState}) {
   return (
     <div className="mt-10 text-3xl mx-auto max-w-6xl">
     <div>Name: remote1</div>
     <div>Framework: react</div>
     <div>Language: JavaScript</div>
     <div>CSS: Tailwind</div>
+    <div className="bg-purple-700 text-white px-5 my-5 py-2 rounded-lg">State: {state}</div>
+    <button className="bg-red-700 text-white px-5 py-2 rounded-lg" onClick={()=>setState(0)}>Clear State</button>
   </div>
   )
 }
